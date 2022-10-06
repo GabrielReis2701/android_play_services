4719d7577ac6cd361cc4f3792d9e8141c2babb9e
GabrielReis2701 <gabrielreis2701@gmail.com>
2022-10-06 16:55:37 -0400

    V2.3.2.1

--- app/src/main/java/org/tensorflow/lite/examples/classification/playservices/SinaisLibras.java
+++ app/src/main/java/org/tensorflow/lite/examples/classification/playservices/SinaisLibras.java
@@ -15,6 +15,7 @@ import java.sql.SQLOutput;
 
 public class SinaisLibras extends AppCompatActivity {
     private static String sinal="";
+    private static int cont=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,25 +28,25 @@ public class SinaisLibras extends AppCompatActivity {
             if(sinal.toUpperCase().equals(resultado.toUpperCase())){
                 AlertDialog.Builder janela = new AlertDialog.Builder(this);
                 janela.setTitle("Resultado da Atividade");
-                janela.setMessage("Parabens vc Acertou!!!!!\n vc Fez a Letra: " + resultado);
+                janela.setMessage("Parabens vc Acertou!!!!!\n vc Fez a Letra: " + resultado+"\nVezes "+cont);
                 janela.setNeutralButton("OK",null);
                 janela.show();
+                sinal="";
             }else{
                 AlertDialog.Builder janela = new AlertDialog.Builder(this);
                 janela.setTitle("Resultado da Atividade");
-                janela.setMessage("Vc deveria fazer a letra: "+sinal+"\n vc Fez a Letra: " + resultado +" \nLetra Incorreta Por Favor Refaça");
+                janela.setMessage("Vc deveria fazer a letra: "+sinal+"\n vc Fez a Letra: " + resultado +" \nLetra Incorreta Por Favor Refaça"+"\nVezes "+cont);
                 janela.setNeutralButton("OK",null);
                 janela.show();
+                sinal="";
             }
         }
     }
     public void onclick(View button){
         sinal= button.getTag().toString();
+        cont=cont+1;
         Intent i = new Intent(SinaisLibras.this,CameraActivity.class);
         i.putExtra("desejado",sinal);
         startActivity(i);
-        button.setTag("");
-
-
     }
 }
